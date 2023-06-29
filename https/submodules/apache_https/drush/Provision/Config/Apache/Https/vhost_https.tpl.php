ffc61bf9a9afba220576aa4143fcf2844f9d492e
Jon Pugh <jon@thinkdrop.net>
2023-06-29 12:58:45 -0400

    Add "platform" directory config to hosting_https template.

--- https/submodules/apache_https/drush/Provision/Config/Apache/Https/vhost_https.tpl.php
+++ https/submodules/apache_https/drush/Provision/Config/Apache/Https/vhost_https.tpl.php
@@ -110,6 +110,27 @@ if ($this->redirection) {
       </IfModule>
     </Directory>
 
+    <Directory <?php print $this->root; ?>>
+      Order allow,deny
+      Allow from all
+      Satisfy All
+      Require all granted
+
+    <?php print $extra_directory_config; ?>
+
+
+    <?php
+    if (is_readable("{$this->root}/.htaccess")) {
+      print "\n# Include the platform's htaccess file\n";
+      print "Include {$this->root}/.htaccess\n";
+    }
+    ?>
+
+      # Do not read any .htaccess in the platform
+      AllowOverride none
+
+    </Directory>
+
   </VirtualHost>
 <?php endif; ?>
 
